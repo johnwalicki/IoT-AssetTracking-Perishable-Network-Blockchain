9a2af3161f050a544dcf5de2cf2c0c5ed325ec06
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-04-09 23:31:46 -0400

    need index.js in smart contract folder

---
+++ Blockchain/IoTsmartContract/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const IoTperishibleContract = require('./lib/IoT-perishable-contract.js');
+
+module.exports.IoTperishibleContract = IoTperishibleContract;
+module.exports.contracts = [ IoTperishibleContract ];
